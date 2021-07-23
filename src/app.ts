c283b1b4333c557c99b68cb237b9f124822ba1c7
Yu Qin <sirius.qinyu@gmail.com>
2021-07-24 00:24:12 +0800

    initial project

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response, NextFunction } from "express";
+import { Express } from "express-serve-static-core";
+import morgan from "morgan";
+import morganBody from "morgan-body";
+
+import config from "@jp/config";
+
+import logger from "@jp/utils/logger";
+
+const setLogger = (app: Express) => {
+    if (config.morganLogger) {
+        const loggerFormat =
+            ":method :url :status :response-time ms - :res[content-length]";
+        app.use(morgan(loggerFormat));
+    }
+
+    if (config.morganBodyLogger) {
+        morganBody(app);
+    }
+};
+
+export async function createApp(): Promise<Express> {
+    const app = express();
+
+    // middleware
+    app.use(express.json()); // 保证 http request body 会被作为 json 传入
+
+    // logger
+    setLogger(app);
+
+    // routes
+
+    // Error-handling middleware: 必须使用 4个 argument
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+        logger.error(err.message);
+        res.status(500).json({ message: err.message });
+    });
+
+    return app;
+}
---
+++ src/config/index.ts
@@ -0,0 +1,45 @@
+import dotenvExtended from "dotenv-extended";
+import dotenvParseVariables from "dotenv-parse-variables";
+
+// winston log levels
+type LogLevel =
+    | "silent" // 默认选项，彻底不用 log
+    | "error"
+    | "warn"
+    | "info"
+    | "http" // prod 环境下，低于这个的不记录
+    | "verbose"
+    | "debug" // dev 环境
+    | "silly";
+
+// env
+const env = dotenvExtended.load({
+    path: process.env.ENV_FILE, // 会在 package.json 内设置这个 ENV_FILE
+    defaults: "./config/.env.defaults", // 即刚才创建的 .env.defaults
+    schema: "./config/.env.schema", // 即刚才创建的 .env.schema
+    includeProcessEnv: true, // 把 process.env 里的变量也放进校验里
+    silent: false, // 没有 .env 或 .env.defaults 文件时，会 console.log 信息
+    errorOnMissing: true, // 缺少 schema 需要的变量时，会报错
+    errorOnExtra: true, // 存在多余的变量时，会报错
+});
+
+const parsedEnv = dotenvParseVariables(env); // parse 环境变量
+
+interface Config {
+    port: number;
+
+    // logger
+    morganLogger: boolean;
+    morganBodyLogger: boolean;
+    // delare loggerLevel config property
+    loggerLevel: LogLevel;
+}
+
+const config: Config = {
+    port: parsedEnv.PORT as number,
+    morganLogger: parsedEnv.MORGAN_LOGGER as boolean,
+    morganBodyLogger: parsedEnv.MORGAN_BODY_LOGGER as boolean,
+    loggerLevel: parsedEnv.LOGGER_LEVEL as LogLevel,
+};
+
+export default config;
